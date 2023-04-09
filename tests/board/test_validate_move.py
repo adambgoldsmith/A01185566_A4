7f17fc6d5c70b64b84e3f7cbfe9c3d6155ea07d2
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-08 19:44:38 -0700

    Create test_validate_move.py and unittests

---
+++ tests/board/test_validate_move.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from board import validate_move
+
+
+class TestValidateMove(TestCase):
+    def test_validate_move_valid(self):
+        board = {(0, 0): 'empty', (0, 1): 'empty', (1, 0): 'empty', (1, 1): 'empty'}
+        character = {'X-coordinate': 0, 'Y-coordinate': 0}
+        direction = 'south'
+        self.assertEqual(True, validate_move(board, character, direction))
+
+    def test_validate_move_invalid(self):
+        board = {(0, 0): 'empty', (0, 1): 'empty', (1, 0): 'empty', (1, 1): 'empty'}
+        character = {'X-coordinate': 0, 'Y-coordinate': 0}
+        direction = 'north'
+        self.assertEqual(False, validate_move(board, character, direction))
