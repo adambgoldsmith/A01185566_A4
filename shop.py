05d15076090815f2ae0177549a9185916da97352
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-08 20:40:12 -0700

    Add doctests and fix PEP errors

--- shop.py
+++ shop.py
@@ -1,5 +1,6 @@
 from character import *
 
+
 def shop(character: dict) -> None:
     """
     Start shop event
@@ -8,11 +9,13 @@ def shop(character: dict) -> None:
     :precondition: character must be a dictionary created by the create_character() function
     :postcondition: Commence the shop event
     """
-    print(f"-------------------------f")
-    print(f"You notice a small trade sfhip gliding through the air. The large wooden propeller on its roof clicks loudly with each rotation.\n"
-          f"You float up next to the shfip, dock, and climb aboard.\n"
-          f"A grubby, short, hobgoblin sfhop keeper welcomes you in...")
+    print(f"-------------------------")
+    print(f"You notice a small trade ship gliding through the air. "
+          f"The large wooden propeller on its roof clicks loudly with each rotation.\n"
+          f"You float up next to the ship, dock, and climb aboard.\n"
+          f"A grubby, short, hobgoblin shop keeper welcomes you in...")
     user_shop_choice(character)
+
 
 def user_shop_choice(character: dict) -> None:
     """
@@ -38,6 +41,7 @@ def user_shop_choice(character: dict) -> None:
         else:
             print(f"I don't know what you mean, friend. Please try again.\n")
 
+
 def sufficient_gold(user_input: str, character: dict) -> None:
     """
     Check gold amount
@@ -47,6 +51,12 @@ def sufficient_gold(user_input: str, character: dict) -> None:
     :precondition: user_input must be a string
     :precondition: character must be a dictionary created by the create_character() function
     :postcondition: Check if user has enough gold to buy an item
+
+    >>> user_input_test = '1'
+    >>> character_test = {'inventory': {'gold': 0}}
+    >>> sufficient_gold(user_input_test, character_test)
+    You don't have enough gold, friend.
+    <BLANKLINE>
     """
     if user_input == '1':
         if character['inventory']['gold'] >= 10:
@@ -59,6 +69,7 @@ def sufficient_gold(user_input: str, character: dict) -> None:
         else:
             print(f"You don't have enough gold, friend.\n")
 
+
 def buy_flux(character: dict) -> None:
     """
     Buy flux
@@ -66,10 +77,18 @@ def buy_flux(character: dict) -> None:
     :param character: A dictionary
     :precondition: character must be a dictionary created by the create_character() function
     :postcondition: Buy flux and reduce gold
+
+    >>> character_test = {'inventory': {'gold': 10, 'flux': 0}}
+    >>> buy_flux(character_test)
+    You bought 2 flux for 10 gold.
+    <BLANKLINE>
+    >>> character_test
+    {'inventory': {'gold': 0, 'flux': 2}}
     """
     character['inventory']['flux'] += 2
     character['inventory']['gold'] -= 10
     print(f"You bought 2 flux for 10 gold.\n")
+
 
 def buy_repair_kit(character: dict) -> None:
     """
@@ -78,6 +97,13 @@ def buy_repair_kit(character: dict) -> None:
     :param character: A dictionary
     :precondition: character must be a dictionary created by the create_character() function
     :postcondition: Buy repair kit and reduce gold
+
+    >>> character_test = {'inventory': {'gold': 30, 'repair_kits': 0}}
+    >>> buy_repair_kit(character_test)
+    You bought a repair kit for 30 gold.
+    <BLANKLINE>
+    >>> character_test
+    {'inventory': {'gold': 0, 'repair_kits': 1}}
     """
     character['inventory']['repair_kits'] += 1
     character['inventory']['gold'] -= 30
