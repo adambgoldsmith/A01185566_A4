d3e5d8c97a09b3e0174b11593e17eb5f65a25bab
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-08 19:50:12 -0700

    Create test_shop.py and unittests

---
+++ tests/shop/test_shop.py
@@ -0,0 +1,17 @@
+import io
+from unittest import TestCase
+from shop import shop
+from unittest.mock import patch
+
+class TestShop(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_shop(self, mock_output):
+        character = {'inventory': {
+            'flux': 0,
+            'repair_kits':0,
+            'gold': 50
+        }}
+        shop(character)
+        shop_output = mock_output.getvalue()
+        expected_output = ""
+        self.assertEqual(expected_output, shop_output)
