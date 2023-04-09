bc052f51898b89e0f91c9374aaf81d0af2e04f93
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-08 19:50:00 -0700

    Create test_buy_repair_kit.py and unittests

---
+++ tests/shop/test_buy_repair_kit.py
@@ -0,0 +1,32 @@
+import io
+from unittest import TestCase
+from shop import buy_repair_kit
+from unittest.mock import patch
+
+class TestBuyRepairKit(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_buy_repair_kit_output(self, mock_output):
+        character = {'inventory': {
+            'repair_kits': 0,
+            'gold': 30
+        }}
+        buy_repair_kit(character)
+        buy_repair_kit_output = mock_output.getvalue()
+        expected_output = "You bought a repair kit for 30 gold.\n\n"
+        self.assertEqual(expected_output, buy_repair_kit_output)
+
+    def test_buy_repair_kit_gold_taken(self):
+        character = {'inventory': {
+            'repair_kits': 0,
+            'gold': 30
+        }}
+        buy_repair_kit(character)
+        self.assertEqual(0, character['inventory']['gold'])
+
+    def test_buy_repair_kit_repair_kit_added(self):
+        character = {'inventory': {
+            'repair_kits': 0,
+            'gold': 30
+        }}
+        buy_repair_kit(character)
+        self.assertEqual(1, character['inventory']['repair_kits'])
