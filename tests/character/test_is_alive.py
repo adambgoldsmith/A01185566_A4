32a5743c3c977eaea74c60ca7e0818ea4774398a
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-08 19:46:44 -0700

    Create test_is_alive.py and unittests

---
+++ tests/character/test_is_alive.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from character import is_alive
+
+
+class TestIsAlive(TestCase):
+    def test_is_alive_true(self):
+        character = {'health': 50}
+        self.assertEqual(True, is_alive(character))
+
+    def test_is_alive_false_zero(self):
+        character = {'health': 0}
+        self.assertEqual(False, is_alive(character))
+
+    def test_is_alive_false_negative(self):
+        character = {'health': -50}
+        self.assertEqual(False, is_alive(character))
