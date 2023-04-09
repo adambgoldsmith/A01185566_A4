0f655c60ee20deb46f2cdaa7a9930eb142e8fff2
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-08 19:50:59 -0700

    Create test_user_shop_choice.py and unittests

---
+++ tests/shop/test_user_shop_choice.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from shop import user_shop_choice
+from unittest.mock import patch
+
+
+class TestUserShopChoice(TestCase):
+    @patch('')
+    def test_user_shop_choice(self):
+        self.fail()
