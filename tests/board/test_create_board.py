c626364e9e668f3d5b022a80a39af2c1bfb41ebd
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-10 12:06:52 -0700

    Fix unittest error

--- tests/board/test_create_board.py
+++ tests/board/test_create_board.py
@@ -12,17 +12,18 @@ class TestCreateBoard(TestCase):
                                          'nymph', 'empty', 'empty', 'chest', 'empty', 'empty',
                                          'empty', 'empty', 'empty', 'chest', 'shop', 'empty',
                                          'nymph', 'empty', 'empty', 'empty', 'empty'])
-    @patch('board.create_events_list', side_effect=[['chest', 'chest', 'nymph', 'nymph', 'shop',
-                                                     'shop', 'empty', 'empty', 'empty', 'empty',
-                                                     'empty', 'empty', 'empty', 'empty', 'empty',
-                                                     'empty', 'empty', 'empty', 'empty', 'empty', 'empty',
-                                                     'empty', 'empty']])
+    @patch('helper_functions.board.create_events_list', side_effect=[['chest', 'chest', 'nymph', 'nymph', 'shop',
+                                                                      'shop', 'empty', 'empty', 'empty', 'empty',
+                                                                      'empty', 'empty', 'empty', 'empty', 'empty',
+                                                                      'empty', 'empty', 'empty', 'empty', 'empty',
+                                                                      'empty',
+                                                                      'empty', 'empty']])
     def test_create_board(self, _, __):
         expected_value = {(0, 0): 'start', (0, 1): 'empty', (0, 2): 'chest', (0, 3): 'empty',
-                               (0, 4): 'empty', (1, 0): 'empty', (1, 1): 'empty', (1, 2): 'empty',
-                               (1, 3): 'chest', (1, 4): 'empty', (2, 0): 'shop', (2, 1): 'nymph',
-                               (2, 2): 'empty', (2, 3): 'shop', (2, 4): 'empty', (3, 0): 'empty',
-                               (3, 1): 'empty', (3, 2): 'empty', (3, 3): 'empty', (3, 4): 'empty',
-                               (4, 0): 'empty', (4, 1): 'empty', (4, 2): 'empty', (4, 3): 'nymph',
-                               (4, 4): 'boss'}
+                          (0, 4): 'empty', (1, 0): 'empty', (1, 1): 'empty', (1, 2): 'empty',
+                          (1, 3): 'chest', (1, 4): 'empty', (2, 0): 'shop', (2, 1): 'nymph',
+                          (2, 2): 'empty', (2, 3): 'shop', (2, 4): 'empty', (3, 0): 'empty',
+                          (3, 1): 'empty', (3, 2): 'empty', (3, 3): 'empty', (3, 4): 'empty',
+                          (4, 0): 'empty', (4, 1): 'empty', (4, 2): 'empty', (4, 3): 'nymph',
+                          (4, 4): 'boss'}
         self.assertEqual(expected_value, create_board(5, 5))
