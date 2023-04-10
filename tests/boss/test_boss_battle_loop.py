c51b522401fcf4332fdf0f1251bbb7ec942cd19f
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-10 12:39:13 -0700

    Add unittests

--- tests/boss/test_boss_battle_loop.py
+++ tests/boss/test_boss_battle_loop.py
@@ -4,11 +4,22 @@ from unittest.mock import patch
 
 
 class TestBossBattleLoop(TestCase):
-    @patch('boss.user_battle_selection', side_effect=[None])
-    @patch('boss.boss_attack', side_effect=[None])
-    @patch('boss.is_alive', side_effect=[True])
+    @patch('helper_functions.boss.user_battle_selection', side_effect=[None])
+    @patch('helper_functions.boss.boss_attack', side_effect=[None])
+    @patch('helper_functions.boss.is_alive', side_effect=[False])
+    def test_boss_battle_loop_character_dead(self, _, __, ___):
+        character = {}
+        boss = {'health': 100}
+        self.assertEqual(None, boss_battle_loop(character, boss))
+
+    @patch('helper_functions.boss.user_battle_selection', side_effect=[None])
+    @patch('helper_functions.boss.boss_attack', side_effect=[None])
+    @patch('helper_functions.boss.is_alive', side_effect=[True])
     def test_boss_battle_loop_character_alive(self, _, __, ___):
         character = {}
-        boss = {}
-        boss_battle_loop(character)
-        self.assertEqual(None, )
+        boss = {'health': 0}
+        self.assertEqual(None, boss_battle_loop(character, boss))
+
+    def test_boss_battle_loop_type_error(self):
+        with self.assertRaises(TypeError):
+            boss_battle_loop(None, None)
