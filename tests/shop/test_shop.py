9b6b1b2bb0c376fb767c59ddc8f67f0e8cfea509
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-09 21:55:59 -0700

    Add unittests

--- tests/shop/test_shop.py
+++ tests/shop/test_shop.py
@@ -9,8 +9,9 @@ from unittest.mock import patch
 
 
 class TestShop(TestCase):
+    @patch('shop.user_shop_choice', side_effect=[None])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_shop(self, mock_output):
+    def test_shop(self, mock_output, _):
         character = {'inventory': {
             'flux': 0,
             'repair_kits': 0,
@@ -18,5 +19,9 @@ class TestShop(TestCase):
         }}
         shop(character)
         shop_output = mock_output.getvalue()
-        expected_output = ""
+        expected_output = "-------------------------\n" \
+                          "You notice a small trade ship gliding through the air. The large wooden propeller on its" \
+                          " roof clicks loudly with each rotation.\n" \
+                          "You float up next to the ship, dock, and climb aboard.\n" \
+                          "A grubby, short, hobgoblin shop keeper welcomes you in...\n"
         self.assertEqual(expected_output, shop_output)
