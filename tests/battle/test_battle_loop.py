7535cc44f5a699918a3ce7c3fcd90c04b460f6f9
adambgoldsmith <adamgoldsmithmail@gmail.com>
2023-04-10 18:19:37 -0700

    Add relevant unittests

--- tests/battle/test_battle_loop.py
+++ tests/battle/test_battle_loop.py
@@ -8,6 +8,20 @@ from unittest.mock import patch
 
 
 class TestBattleLoop(TestCase):
-    @patch('itertools.cycle', )
-    def test_battle_loop(self):
-        self.fail()
+    @patch('itertools.cycle', side_effect=['character', 'enemy'])
+    @patch('helper_functions.character.is_alive', side_effect=[True])
+    def test_battle_loop_character_alive(self, _, __):
+        character = {'health': 100}
+        enemy = {'health': 0}
+        self.assertEqual(None, battle_loop(character, enemy))
+
+    @patch('itertools.cycle', side_effect=['character', 'enemy'])
+    @patch('helper_functions.character.is_alive', side_effect=[False])
+    def test_battle_loop_character_dead(self, _, __):
+        character = {'health': 0}
+        enemy = {'health': 0}
+        self.assertEqual(None, battle_loop(character, enemy))
+
+    def test_battle_loop_type_error(self):
+        with self.assertRaises(TypeError):
+            battle_loop(None, None)
